c48dff345184471b676a6c739bf0050050ceb767
HUIMIN TAO <htao@uoc.edu>
2024-10-27 20:23:53 +0100

    proyecto PEC1

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,16 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+
+
+// Configuración del menú de hamburguesa
+const hamburguesa = document.querySelector(".header__hamburguesa");
+const navlinks = document.querySelector(".header__navigation");
+
+if (hamburguesa && navlinks) {
+    hamburguesa.addEventListener("click", () => {
+        navlinks.classList.toggle("active");
+    });
+} else {
+    console.error("Elementos del menú de hamburguesa no encontrados en el DOM.");
+}
